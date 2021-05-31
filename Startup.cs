d87a5fa39262686e9f360602f5ff6f76901e5aa1
Muhtasim-Fuad-Showmik <49075537+Fuad-Hellscream@users.noreply.github.com>
2021-05-31 12:51:11 +0600

    First commit

---
+++ Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BoysToys.Startup))]
+namespace BoysToys
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
