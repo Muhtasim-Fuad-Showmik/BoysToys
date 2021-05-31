760d99a39bed0738473323c8fa33485397e528db
Muhtasim-Fuad-Showmik <49075537+Fuad-Hellscream@users.noreply.github.com>
2021-05-31 15:39:24 +0600

    Display Data Items and Details
    Added markup and code to display a product list and to display product details.

--- ProductList.aspx.cs
+++ ProductList.aspx.cs
@@ -4,14 +4,25 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using BoysToys.Models;
+using System.Web.ModelBinding;
 
 namespace BoysToys
 {
     public partial class ProductList : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
-        {
+        { }
 
+        public IQueryable<Product> GetProducts([QueryString("id")] int? categoryId)
+        {
+            var _db = new BoysToys.Models.ProductContext();
+            IQueryable<Product> query = _db.Products;
+            if (categoryId.HasValue && categoryId > 0)
+            {
+                query = query.Where(p => p.CategoryID == categoryId);
+            }
+            return query;
         }
     }
 }
