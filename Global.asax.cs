f232af99f813bc89a1b2225442bb9b3c0062d622
Muhtasim-Fuad-Showmik <49075537+Fuad-Hellscream@users.noreply.github.com>
2021-05-31 13:05:05 +0600

    Created the Data Access Layer
    Created the data model, as well as, added the code that will be used to initialize and seed the database. Configured the application to use the data models when the application is run.

--- Global.asax.cs
+++ Global.asax.cs
@@ -6,6 +6,8 @@ using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
+using System.Data.Entity;
+using BoysToys.Models;
 
 namespace BoysToys
 {
@@ -16,6 +18,9 @@ namespace BoysToys
             // Code that runs on application startup
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Initialize the product database.
+            Database.SetInitializer(new ProductDatabaseInitializer());
         }
     }
 }
---
+++ Models/ProductContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace BoysToys.Models
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext() : base("BoysToys")
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
